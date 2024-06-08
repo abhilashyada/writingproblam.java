fbeee0ee96141210e4a407d68dde3e2e2fc7a437
YADA ABHILASH <108734672+abhilashyada@users.noreply.github.com>
2024-06-08 20:22:27 +0530

    Add files via upload

---
+++ writingprogram.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class main {
+    public static void main(String[] args){
+        Scanner abhi = new Scanner(System.in);
+        int abhilash = abhi.nextInt();
+        System.out.println(abhilash);
+        abhi.close();
+    }
+}
